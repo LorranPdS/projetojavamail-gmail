c55fcc45a9dcd3c402a595841563dbb705c04e5f
LorranPdS <lorransantospereira@yahoo.com.br>
2020-10-28 10:12:06 -0300

    Envio de e-mail para mais de um destinatário

--- src/main/java/projetojavamail_gmail/projetojavamail_gmail/ObjetoEnviarEmail.java
+++ src/main/java/projetojavamail_gmail/projetojavamail_gmail/ObjetoEnviarEmail.java
@@ -1,5 +1,7 @@
 package projetojavamail_gmail.projetojavamail_gmail;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Address;
@@ -13,22 +15,22 @@ import javax.mail.internet.MimeMessage;
 
 public class ObjetoEnviarEmail {
 
-	private String loginUsuario = "empresaa1@gmail.com";
-	private String senhaUsuario = "senhaempresa";
+	private String loginUsuario = "emailremetente@gmail.com";
+	private String senhaUsuario = "senharemetente";
 
-	private String listaDestinatarios = "";
+	private List<String> listaDestinatarios = Arrays.asList("destinatario1@gmail.com"
+			, "destinatario2@yahoo.com.br");
 	private String nomeRemetente = "";
 	private String assuntoEmail = "";
 	private String textoEmail = "";
 
-	public ObjetoEnviarEmail(String listaDestinatarios, String nomeRemetente, String assuntoEmail, String textoEmail) {
-		this.listaDestinatarios = listaDestinatarios;
+	public ObjetoEnviarEmail(String nomeRemetente, String assuntoEmail, String textoEmail) {
 		this.nomeRemetente = nomeRemetente;
 		this.assuntoEmail = assuntoEmail;
 		this.textoEmail = textoEmail;
 	}
-	
-	public void enviarEmail() throws Exception, Exception {
+
+	public void enviarEmail() throws Exception {
 		Properties properties = new Properties();
 		properties.put("mail.smtp.connectiontimeout", "1000");
 		properties.put("mail.smtp.timeout", "5000");
@@ -38,7 +40,8 @@ public class ObjetoEnviarEmail {
 		properties.put("mail.smtp.host", "smtp.gmail.com"); // Servidor Gmail
 		properties.put("mail.smtp.port", "465"); // Porta do servidor
 		properties.put("mail.smtp.socketFactory.port", "465"); // Especifica a porta a ser conectada pelo socket
-		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // Classe socket de conexão ao SMTP
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // Classe socket de conexão
+																							// ao SMTP
 
 		Session session = Session.getInstance(properties, new Authenticator() {
 			@Override
@@ -46,19 +49,24 @@ public class ObjetoEnviarEmail {
 				return new PasswordAuthentication(loginUsuario, senhaUsuario);
 			}
 		});
+
+		int cont = 0;
+		for (String destino : listaDestinatarios) {
 		
-		Address[] toUser = InternetAddress.parse(listaDestinatarios);
-		
+		Address[] toUser = InternetAddress.parse(destino);
+
 		Message message = new MimeMessage(session);
 		message.setFrom(new InternetAddress(loginUsuario, nomeRemetente));
 		message.setRecipients(Message.RecipientType.TO, toUser);
 		message.setSubject(assuntoEmail);
 		message.setText(textoEmail);
 		
-		System.out.println("Enviando...");
+		cont++;
+		System.out.println("Enviando para destinatário " + cont);
 		Transport.send(message);
-		System.out.println("Mensagem enviada com sucesso!");
-		
+		}
+		System.out.println("Mensagens enviadas com sucesso!");
+
 	}
 
 }
--- src/test/java/projetojavamail_gmail/projetojavamail_gmail/AppTest.java
+++ src/test/java/projetojavamail_gmail/projetojavamail_gmail/AppTest.java
@@ -2,26 +2,14 @@ package projetojavamail_gmail.projetojavamail_gmail;
 
 import org.junit.Test;
 
-/*AVISO IMPORTANTE:
- 
-  Para que o projeto funcione, siga os seguintes passos dentro do seu Gmail:
-  
-  1. Acesse Manage your Google Account (Gerenciar sua conta do Google);
-  2. A seguir, clique em Segurança;
-  3. Em 'Acesso a app menos seguro', clique em 'Ativar acesso'; e
-  4. Por fim, ative clicando em 'Permitir aplicativos menos seguros'.
-*/
 public class AppTest {
 
 	@Test
 	public void testeEmail() throws Exception {
 
-		ObjetoEnviarEmail email = new ObjetoEnviarEmail(
-			"destinatario1@yahoo.com.br",
-			"Empresa A1",
-			"Envio de email usando Java",
-			"Olá developer! Esse é um teste de envio de email usando JavaMail!");
-		
+		ObjetoEnviarEmail email = new ObjetoEnviarEmail("emailremetente@gmail.com", "Teste Email",
+				"Email enviado com sucesso sem que você saiba mais quem além de você recebeu!");
+
 		email.enviarEmail();
 	}
 }
